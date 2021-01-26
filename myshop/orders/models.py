86cdd59ad10d0f8db5b63975ddfe6738c6b95031
Kacper Stasica <kacperstasica@yahoo.pl>
2021-01-26 18:01:43 +0100

    Add fields coupon and discount to order

--- myshop/orders/models.py
+++ myshop/orders/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 from shop.models import Product
@@ -14,6 +15,14 @@ class Order(models.Model):
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
     braintree_id = models.CharField(max_length=150, blank=True)
+    coupon = models.ForeignKey("coupons.Coupon",
+                               related_name='orders',
+                               null=True,
+                               blank=True,
+                               on_delete=models.SET_NULL)
+    discount = models.IntegerField(default=0,
+                                   validators=[MinValueValidator(0),
+                                               MaxValueValidator(100)])
 
     class Meta:
         ordering = ('-created',)
